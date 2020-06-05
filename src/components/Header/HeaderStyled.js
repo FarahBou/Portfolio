6afdf69b00655ccf81524b93ee784693b97a14c4
Live session user <mint@etudiant.oclock.io>
2020-06-05 18:17:45 +0200

    header responsive 2

--- src/components/Header/HeaderStyled.js
+++ src/components/Header/HeaderStyled.js
@@ -18,6 +18,28 @@ p {
 }
 
 @media only screen and (max-device-width: 520px){
+  left: 20px;
+  p {
+    padding: 0 9px;
+    color: #F0EBD8 ;
+    font-size: 1em;
+    font-family: 'Megrim', cursive;
+    text-shadow: 1px 1px 3px black; 
+  }
+}
+
+@media only screen and (max-device-width: 420px){
+  left: 13px;
+  p {
+    padding: 0 7px;
+    color: #F0EBD8 ;
+    font-size: 0.9em;
+    font-family: 'Megrim', cursive;
+    text-shadow: 1px 1px 3px black; 
+  }
+}
+
+@media only screen and (max-device-width: 360px){
   left: 13px;
   p {
     padding: 0 5px;
