9be5963a8cbaba054c62f6189d69bcf24f8fbaf8
Live session user <mint@etudiant.oclock.io>
2020-06-05 20:36:38 +0200

    BG animation

--- src/components/Contact/ContactStyled.js
+++ src/components/Contact/ContactStyled.js
@@ -2,12 +2,26 @@ import styled from 'styled-components';
 
 export default styled.div`  
 min-height: 100vh;
-background: linear-gradient(#1D2D44, #38040E);
+background: linear-gradient(-45deg, #38040E, #38040E, #1D2D44, #0D1321);
+background-size: 400% 400%;
+animation: gradient 15s ease infinite;
 overflow: hidden;
 align-items: center;
 display: flex;
 flex-direction: column;
 justify-content: center;
+
+@keyframes gradient {
+	0% {
+	  background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+}
 
 #header {
   z-index: 1;
