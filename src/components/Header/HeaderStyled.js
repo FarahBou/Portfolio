93ee01379bb8a170076f1e00fcbd49d34aaa6060
Live session user <mint@etudiant.oclock.io>
2020-06-05 18:11:02 +0200

    header responsive

--- src/components/Header/HeaderStyled.js
+++ src/components/Header/HeaderStyled.js
@@ -17,4 +17,15 @@ p {
   text-shadow: 1px 1px 3px black; 
 }
 
+@media only screen and (max-device-width: 520px){
+  left: 13px;
+  p {
+    padding: 0 5px;
+    color: #F0EBD8 ;
+    font-size: 0.8em;
+    font-family: 'Megrim', cursive;
+    text-shadow: 1px 1px 3px black; 
+  }
+}
+
 `;
