cca1d5c7d160b69cdf74012d5c8b59897273ca21
FarahBou <f.bouzbib@gmail.com>
2020-07-16 23:14:31 +0200

    test deploy

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 
 // serve up production assets
-app.use(express.static('client/dist'));
+app.use(express.static('../client/dist'));
 
 // let the react app to handle any unknown routes 
 // serve up the index.html if express does'nt recognize the route
 app.get('*', (req, res) => {
-res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+res.sendFile(path.resolve('../client/dist/index.html'));
 });
 
 // if not in production use the port 5000
