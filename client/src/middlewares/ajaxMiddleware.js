b9c153e8799ee4a17f3fbaad4e14b6187bdbfeb9
FarahBou <f.bouzbib@gmail.com>
2020-07-21 23:06:43 +0200

    inbound

--- client/src/middlewares/ajaxMiddleware.js
+++ client/src/middlewares/ajaxMiddleware.js
@@ -25,7 +25,7 @@ const ajaxMiddleware = (store) => (next) => (action) => {
 
       axios({
         method: 'post',
-        url: 'http://18.210.15.139:3002/Contact/send',
+        url: 'http://18.210.15.139:3002/send',
         data: {
           name: state.name,
           email: state.email,
