e81f27c13fa6f6e2cb44866547dc2de2a7d51e0f
FarahBou <f.bouzbib@gmail.com>
2020-12-08 15:05:24 +0100

    port 80

--- server/index.js
+++ server/index.js
@@ -12,6 +12,6 @@ res.sendFile(path.resolve('../client/dist/index.html'));
 });
 
 // if not in production use the port 5000
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 console.log('server started on port:',PORT);
 app.listen(PORT);
